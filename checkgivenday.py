c51e369d7bb7dbed504efeee822e9b735701b906
SangeethRaj <sangeethraj3498@gmail.com>
2017-08-10 14:11:41 +0530

    Create checkgivenday.py

---
+++ checkgivenday.py
@@ -0,0 +1,15 @@
+days=input()
+if(days=='Monday'):
+    print ("true")
+elif(days=='Tuesday'):
+     print("true")
+elif(days=='Wednesday'):
+     print("true")
+elif(days=='Thursday'):
+     print("true")
+elif(days=='Friday'):
+     print("true")
+elif(days=='Saturday'):
+     print("False")
+elif(days=='Sunday'):
+     print("False")
