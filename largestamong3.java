a800a7451319ca6d3cf21357a9db793e3fa05aaf
SangeethRaj <sangeethraj3498@gmail.com>
2017-08-10 09:13:59 +0530

    Create largestamong3.java

---
+++ largestamong3.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+{
+class Largestamong3
+{
+public static void main(Strings[] args)
+
+if ( x > y && x > z )
+         System.out.println("First number is largest.");
+      else if ( y > x && y > z )
+         System.out.println("Second number is largest.");
+      else if ( z > x && z > y )
+         System.out.println("Third number is largest.");
+      else   
+         System.out.println("Entered numbers are not distinct.");
