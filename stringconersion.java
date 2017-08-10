231d4e8c2c66ef9609032159cc353fbdaddaeab8
SangeethRaj <sangeethraj3498@gmail.com>
2017-08-10 14:32:43 +0530

    Create stringconersion.java

---
+++ stringconersion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class stringconversion
+{
+	public static void main(String args[])
+  {
+	Scanner scan=new Scanner(System.in);
+	String str1=scan.next();
+	for(int i=0;i<str1.length();i++)
+  {
+	char ch=str1.charAt(i);
+	if(Character.isLowerCase(ch))
+  {
+		System.out.println(Character.toUpperCase(ch));
+	}
+	else
+  {
+		System.out.println(Character.toLowerCase(ch));
+	}		
+	}
+	}
+  }
