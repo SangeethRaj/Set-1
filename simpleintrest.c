9284554bc06c94301a4ab8d1f26c9bfa74135059
Sangeethraj 100 <sangeethraj3498@gmail.com>
2017-08-10 14:50:08 +0530

    Create simpleintrest.c

---
+++ simpleintrest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int p,n,r;
+float intrest;
+printf("Enter the principle");
+scanf("%d",&p);
+printf("Enter the no of years");
+scanf("%d",&n);
+printf("Enter the rate of intrest")
+scanf("%d",&r);
+intrest=(p*n*r)/100;
+printf("%f",intrest);
+return0;
+}
