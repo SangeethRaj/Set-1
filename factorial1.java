94bac225d4cc5dfe0a3412be760e13d6e7748b40
Sangeethraj 100 <sangeethraj3498@gmail.com>
2017-09-14 09:03:05 +0530

    Update factorial1.java

--- factorial1.java
+++ factorial1.java
@@ -1,13 +1,13 @@
-class Factorial
+class Fact
 {  
  public static void main(String args[])
  {  
-  int i,fact=1;  
+  int i,f=1;  
   int num=5;
   for(i=1;i<=num;i++)
   {    
-      fact=fact*i;    
+      f=f*i;    
   }    
-  System.out.println("Factorial of "+num+" is: "+fact);    
+  System.out.println("Factorial of "+num+" is: "+f);    
  }  
 }  
